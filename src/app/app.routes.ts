24204e11e82e0bc21de34c4c3d333991535d5f87
Eugenio Baglieri <eugenio.baglieri@skylabs.it>
2026-04-21 15:31:59 +0200

    Update routing to set 'product-preview-card' as the default path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    //path: 'product-preview-card',
-    path: '',
+    path: 'product-preview-card',
     loadComponent: () =>
       import('./pages/product-preview-card/product-preview-card').then((m) => m.ProductPreviewCard),
   },
