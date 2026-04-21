a1683dbcd75fb504284c17dae9219c6e3e143aa6
Eugenio Baglieri <eugenio.baglieri@skylabs.it>
2026-04-21 18:20:49 +0200

    WIP

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'product-preview-card',
+    path: '',
     loadComponent: () =>
       import('./pages/product-preview-card/product-preview-card').then((m) => m.ProductPreviewCard),
   },
