a86bcb779036cca434299b0fdb168578e94f161b
Eugenio Baglieri <eugenio.baglieri@skylabs.it>
2026-04-21 21:17:45 +0200

    move site in its path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'product-preview-card',
     loadComponent: () =>
       import('./pages/product-preview-card/product-preview-card').then((m) => m.ProductPreviewCard),
   },
